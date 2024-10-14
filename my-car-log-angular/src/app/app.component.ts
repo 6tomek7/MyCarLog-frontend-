7da1eabb8ab9e42e8a7c9e26b08ec16e81894f26
Tom <krzysiekwichura@gmail.com>
2024-10-14 11:44:00 +0200

    initial functionality for top bar

--- my-car-log-angular/src/app/app.component.ts
+++ my-car-log-angular/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TranslocoPipe } from '@jsverse/transloco';
+import { TopBarComponent } from './core/layout/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TranslocoPipe],
+  imports: [RouterOutlet, TopBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'my-car-log-angular';
-}
+export class AppComponent {}
