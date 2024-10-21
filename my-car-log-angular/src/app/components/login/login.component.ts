7b3604fe593d5c3fcf7e7669b072fd754ca17a51
Tom <krzysiekwichura@gmail.com>
2024-10-21 19:55:30 +0200

    add error handle for login

--- my-car-log-angular/src/app/components/login/login.component.ts
+++ my-car-log-angular/src/app/components/login/login.component.ts
@@ -7,30 +7,48 @@ import {
 } from '@angular/forms';
 import { UserService } from '../../services/user.service';
 import { LoginModel } from '../../models/user/login.model';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(private userService: UserService) {}
+  login = new FormControl('', Validators.required);
+  password = new FormControl('', Validators.required);
 
   myForm = new FormGroup({
-    login: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
+    login: this.login,
+    password: this.password,
   });
+  constructor(private userService: UserService, private router: Router) {}
 
   onSubmit() {
     const login: LoginModel = {
-      username: this.myForm.get('login')?.value as string,
-      password: this.myForm.get('password')?.value as string,
+      username: this.login.value as string,
+      password: this.password.value as string,
     };
 
-    this.userService.signIn(login).subscribe((res) => {
-      console.log('test');
+    this.userService.signIn(login).subscribe({
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        err.error.message === 'Invalid password'
+          ? this.password.setErrors({ notMatch: true })
+          : this.login.setErrors({ notExist: true });
+      },
     });
   }
 }
