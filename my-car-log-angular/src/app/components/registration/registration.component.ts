21fc9d421a0296339e14ec6f86cce09ad3b4912f
Tom <krzysiekwichura@gmail.com>
2024-10-21 13:49:48 +0200

    added unsubscribe for registration

--- my-car-log-angular/src/app/components/registration/registration.component.ts
+++ my-car-log-angular/src/app/components/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -12,6 +12,8 @@ import { RegistrationDataModel } from '../../models/user/registrationData.model'
 import { CommonModule } from '@angular/common';
 import { confirmPasswordValidator } from '../../shared/validators/confirm-password.validator';
 import { Router } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
@@ -21,11 +23,16 @@ import { Router } from '@angular/router';
     MatFormFieldModule,
     ReactiveFormsModule,
     CommonModule,
+    MatProgressBarModule,
   ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnDestroy {
+  private ngUnsubscribe = new Subject();
+
+  loading = false;
+
   usernameValidators = { minLength: 5, maxLength: 25 };
   passwordValidators = { minLength: 8, maxLength: 25 };
   username = new FormControl('', [
@@ -41,7 +48,6 @@ export class RegistrationComponent {
   ]);
   passwordConfirmation = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
-
   myForm = new FormGroup(
     {
       username: this.username,
@@ -54,6 +60,11 @@ export class RegistrationComponent {
 
   constructor(private userService: UserService, private router: Router) {}
 
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next({});
+    this.ngUnsubscribe.complete();
+  }
+
   onSubmit() {
     const registrationDataModel: RegistrationDataModel = {
       username: this.username.value as string,
@@ -61,17 +72,27 @@ export class RegistrationComponent {
       email: this.email.value as string,
     };
 
-    this.userService.signUp(registrationDataModel).subscribe({
-      next: () => {
-        this.router.navigate(['/home']);
-      },
-      error: (err) => {
-        err.error.message === 'Username already exists' &&
-          this.username.setErrors({ alreadyExist: true });
+    this.loading = true;
 
-        err.error.message === 'Email already exists' &&
-          this.email.setErrors({ alreadyExist: true });
-      },
-    });
+    this.userService
+      .signUp(registrationDataModel)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/home']);
+          this.loading = false;
+        },
+        error: (err) => {
+          this.loading = false;
+          err.error.message === 'Username already exists' &&
+            this.username.setErrors({ alreadyExist: true });
+
+          err.error.message === 'Email already exists' &&
+            this.email.setErrors({ alreadyExist: true });
+        },
+        complete: () => {
+          this.loading = false;
+        },
+      });
   }
 }
