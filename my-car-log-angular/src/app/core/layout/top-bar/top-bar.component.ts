e9dced7795333d812a1eb0934b363784457a151f
Tom <krzysiekwichura@gmail.com>
2024-10-28 13:31:30 +0100

    add language selection

--- my-car-log-angular/src/app/core/layout/top-bar/top-bar.component.ts
+++ my-car-log-angular/src/app/core/layout/top-bar/top-bar.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
-import { TranslocoDirective } from '@jsverse/transloco';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [MatIconModule, RouterModule, TranslocoDirective],
+  imports: [MatIconModule, RouterModule, TranslocoDirective, FormsModule],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
-export class TopBarComponent {}
+export class TopBarComponent implements OnInit {
+  selectedLanguage = 'pl';
+  constructor(private translocoService: TranslocoService) {}
+  ngOnInit(): void {
+    this.changeLanguage();
+  }
+
+  changeLanguage() {
+    this.translocoService.setActiveLang(this.selectedLanguage);
+  }
+}
