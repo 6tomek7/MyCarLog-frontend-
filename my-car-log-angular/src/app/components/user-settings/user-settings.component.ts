df105172ea9aed9cc0012835d7034791535eaaf7
Tom <krzysiekwichura@gmail.com>
2024-10-30 17:09:42 +0100

    add inject aditional components for UserSettingsComponent

--- my-car-log-angular/src/app/components/user-settings/user-settings.component.ts
+++ my-car-log-angular/src/app/components/user-settings/user-settings.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SecuritySettingsComponent } from './security-settings/security-settings.component';
+import { GeneralSettingsComponent } from './general-settings/general-settings.component';
+import { AdditionalSettingsComponent } from './additional-settings/additional-settings.component';
 
 @Component({
   selector: 'app-user-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule,
+    SecuritySettingsComponent,
+    GeneralSettingsComponent,
+    AdditionalSettingsComponent,
+  ],
   templateUrl: './user-settings.component.html',
-  styleUrl: './user-settings.component.scss'
+  styleUrl: './user-settings.component.scss',
 })
-export class UserSettingsComponent {
-
-}
+export class UserSettingsComponent {}
