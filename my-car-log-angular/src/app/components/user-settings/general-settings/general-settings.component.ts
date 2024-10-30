22a28a3eec91f36c6e8a0732408ef5680a12a048
Tom <krzysiekwichura@gmail.com>
2024-10-30 17:53:55 +0100

    add LanguageOptionComponent

--- my-car-log-angular/src/app/components/user-settings/general-settings/general-settings.component.ts
+++ my-car-log-angular/src/app/components/user-settings/general-settings/general-settings.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { LanguageOptionComponent } from '../../../core/language-option/language-option.component';
 
 @Component({
   selector: 'app-general-settings',
   standalone: true,
-  imports: [],
+  imports: [TranslocoDirective, MatSlideToggleModule, LanguageOptionComponent],
   templateUrl: './general-settings.component.html',
-  styleUrl: './general-settings.component.scss'
+  styleUrl: './general-settings.component.scss',
 })
-export class GeneralSettingsComponent {
-
-}
+export class GeneralSettingsComponent {}
