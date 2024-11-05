dc833d9558f757848ba29888cc1307064e9561e4
Tom <krzysiekwichura@gmail.com>
2024-11-05 15:22:09 +0100

    add translocoDirective for UserSettingsComponennt

--- my-car-log-angular/src/app/components/user-settings/user-settings.component.ts
+++ my-car-log-angular/src/app/components/user-settings/user-settings.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { SecuritySettingsComponent } from './security-settings/security-settings.component';
 import { GeneralSettingsComponent } from './general-settings/general-settings.component';
 import { AdditionalSettingsComponent } from './additional-settings/additional-settings.component';
+import { UserService } from '../../services/user.service';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-user-settings',
@@ -12,8 +14,17 @@ import { AdditionalSettingsComponent } from './additional-settings/additional-se
     SecuritySettingsComponent,
     GeneralSettingsComponent,
     AdditionalSettingsComponent,
+    TranslocoDirective,
   ],
   templateUrl: './user-settings.component.html',
   styleUrl: './user-settings.component.scss',
 })
-export class UserSettingsComponent {}
+export class UserSettingsComponent implements OnInit {
+  userService = inject(UserService);
+
+  ngOnInit(): void {}
+
+  getUserDetails(): void {
+    this.userService.getUserDetails().subscribe(() => {});
+  }
+}
